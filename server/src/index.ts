4f59a222f206be2605fa14e617fddaef01b668af
maytham okal <maythamokal@gmail.com>
2025-11-12 15:21:27 +1100

    adding new sample file

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,11 +9,12 @@ app.get('/', (req, res) => {
 })
 
 app.post('/submit', (req, res) => {
-  console.log('Received:', req.body.input)
-  res.json({ message: `Server received: ${req.body.input}` })
+
+ const input = req.body.name
+
+  res.json({ message: `Server received: ${input}` })
 })
 
 
-///////// tis is a test for auto git /////
 
 app.listen(3000, () => console.log('Server running on port 3000'))
